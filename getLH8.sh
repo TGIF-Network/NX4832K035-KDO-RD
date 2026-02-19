f8e539fc980b755596cd211adf5b459d233ddaec
root <root@pi-star>
2026-02-18 20:20:40 -0500

    Update

--- getLH8.sh
+++ getLH8.sh
@@ -28,6 +28,9 @@ call1=""
 name=""
 #line=list1
 #echo "$line"
+line3=""
+line0=""
+line7=""
 while read -r line
 do
 	mode=$(echo "$line"| cut -d ' ' -f 4 | tr -d ',')
@@ -57,9 +60,13 @@ fi
 
 	tm=$(echo "$line" | cut -d' ' -f3)
 	dt=$(echo "$line" | cut -d' ' -f2)
-	
-	tm1=$(date -d "${tm:0:-1} UTC" '+%R')
-	tm=${tm1:0:5}
+	dtm="$dt ""$tm"
+
+#	tm1=$(date -d "${tm:0:-1} UTC" '+%R')
+#	tm=${tm1:0:5}
+	loc=$(date -d "${dtm:0} UTC" '+%Y-%m-%d %H:%M:%S')
+	tm=$(date -d "${dtm:0} UTC" '+%H:%M:%S')
+# echo "$loc"
 #	dt=${dt:5:5}
 	pl=$(echo "$line" | cut -d' ' -f20)
 	dur=$(echo "$line" | cut -d' ' -f18)
@@ -71,18 +78,30 @@ fi
         call1=$(echo "$dataline" | cut -d',' -f2 | head -1)
 
         name=$(echo "$dataline" | cut -d',' -f3 | head -1)
-        city=$(echo "$dataline" | cut -d',' -f4 | head -1)
-        prov=$(echo "$dataline" | cut -d',' -f5 | head -1)
+#        city=$(echo "$dataline" | cut -d',' -f4 | head -1)
+#        prov=$(echo "$dataline" | cut -d',' -f5 | head -1)
         country=$(echo "$dataline" | cut -d',' -f7 | head -1)
 
 	if [ -z "$name" ]; then
 		name="Name N/A"
 	fi
+#echo  "$tm $mode $call $name $country |"
+#line0="$tm $mode $call $name $country |"
+line0="$tm $mode $call $name $country ""\n"
 
+#echo -e "---$line0|""\n"
+
+      if [ "$mode" == "YSF" ]; then
+	line0="$tm $mode $call""|"
+      fi
+	
+	line7+="$line0"
+
+#	echo -e "$line7""\n"
 
 #	line3=$(echo  "$dt" "$tm" "$call" "$name" | awk '{printf  "%5s %s %s %s|\n", $1 $2 $3 $name}')
-	line3=""
-	echo  "$tm $mode $call $name $country |" |  tr -d "\n"
+#	line3=""
+#	echo  "$tm $mode $call $name $country |" |   tr -d "\n"
 #	echo "$line2"
 #	line4=${line3:0:37}
 	line6=$(echo "$line3" | tr -d "\n")
@@ -90,8 +109,10 @@ fi
 	list5+="$line6"
 done <<< "$list1"
 
+echo -e "$line7"
+
 var="${list5:0:400}"
-#echo "${var}"
+echo "${var}"
 #sudo mount -o remount,rw /
 
 #echo "${var}" >> ./lh2_start.txt
@@ -108,9 +129,10 @@ f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
 #list2=$(tail -n 100 /var/log/pi-star/MM* | grep 'transmission from' |  awk '{seen[$14]=$0} END {for (key in seen) print seen[key]}' | sort -k3n)
 #list2=$(tail -n 200 /var/log/pi-star/MM* | grep 'transmission from' |  awk '{seen[$14]=$0} END {for (key in seen) print seen[key]}')
 list2=$(tail -n 200 /var/log/pi-star/MM* 2>/dev/null |
-        grep -E 'transmission from|received network data from' |
+        grep -E 'voice transmission from|received network data from' |
         awk '!seen[$14]++ {print $0}' )
 
+#echo "$list2"
 
 list3=$(echo "$list2" | sort -k2 -k3,1nr | tail -n 20)
 list1=$(echo "$list3" | tail -n 8)
@@ -118,9 +140,19 @@ list1=$(echo "$list3" | tail -n 8)
 
 #echo "5 $list1"
 #echo "  "
-line=$(echo "$list1" | tail -n8)
+line=$(echo "$list1" | tail -n10)
 #echo "6 $list1"
+
 	domode2
 
 #sudo mount -o remount,ro /
 
+
+
+#echo "Input UTC Taken from last entry in MMDVMHost Log file"
+#echo "Input $input"
+#loc=$(date -d "${input:0} UTC" '+%Y-%m-%d %H:%M:%S')
+#echo "Local $loc"
+#echo "  convert back to UTC "
+#utc2=$(date -d "${loc:0}" +%s | xargs -I {} date -u -d "@{}" '+%Y-%m-%d %H:%M:%S')
+#echo "UTC $utc2" 
