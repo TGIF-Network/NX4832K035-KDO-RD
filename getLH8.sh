269075553bfb1837b3079279892ea604e47b0869
VE3ZRD <root@pi-star>
2026-02-19 08:50:32 -0500

    fixed YSF

--- getLH8.sh
+++ getLH8.sh
@@ -34,24 +34,24 @@ line7=""
 while read -r line
 do
 	mode=$(echo "$line"| cut -d ' ' -f 4 | tr -d ',')
-if [ "$mode" == "DMR" ]; then
+if [ "$mode" = "DMR" ]; then
 	call=$(echo "$line" | cut -d' ' -f14)
 fi
-if [ "$mode" == "D-Star" ]; then
+if [ "$mode" = "D-Star" ]; then
 	call=$(echo "$line" | cut -d' ' -f11)
 fi
-if [ "$mode" == "P25" ]; then
+if [ "$mode" = "P25" ]; then
 	call=$(echo "$line" | cut -d' ' -f9)
 fi
-if [ "$mode" == "NXDN" ]; then
+if [ "$mode" = "NXDN" ]; then
 	call=$(echo "$line" | cut -d' ' -f11)
 fi
-if [ "$mode" == "M17" ]; then
+if [ "$mode" = "M17" ]; then
 	call=$(echo "$line" | cut -d' ' -f11)
 fi
-if [ "$mode" == "YSF" ]; then
+if [ "$mode" = "YSF" ]; then
 	call=$(echo "$line" | cut -d' ' -f11)
-	if [ "$call" == "" ]; then
+	if [ "$call" = "" ]; then
 #		call=$(echo "$line" | cut -d' ' -f16)
 		call=$(echo "$line" | cut -d' ' -f9)
 	fi
@@ -61,12 +61,10 @@ fi
 	tm=$(echo "$line" | cut -d' ' -f3)
 	dt=$(echo "$line" | cut -d' ' -f2)
 	dtm="$dt ""$tm"
-
-#	tm1=$(date -d "${tm:0:-1} UTC" '+%R')
-#	tm=${tm1:0:5}
-	loc=$(date -d "${dtm:0} UTC" '+%Y-%m-%d %H:%M:%S')
-	tm=$(date -d "${dtm:0} UTC" '+%H:%M:%S')
-# echo "$loc"
+	
+	tm1=$(date -d "${tm:0:-1} UTC" '+%R')
+	tm=${tm1:0:5}
+#	loc=$(dt -d "${input:0} UTC" '+%Y-%m-%d %H:%M:%S')
 #	dt=${dt:5:5}
 	pl=$(echo "$line" | cut -d' ' -f20)
 	dur=$(echo "$line" | cut -d' ' -f18)
@@ -74,34 +72,46 @@ fi
 
 #	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
 	dataline=$(sudo sed -n "/$call/p" /usr/local/etc/stripped2.csv)
+
+city=""
+name="NoName"
+prov=""
+country=""
+
+if [ ! -z "$dataline" ]; then
+#echo "$dataline"
        	did=$(echo "$dataline" | cut -d',' -f1 | head -1)
         call1=$(echo "$dataline" | cut -d',' -f2 | head -1)
 
         name=$(echo "$dataline" | cut -d',' -f3 | head -1)
-#        city=$(echo "$dataline" | cut -d',' -f4 | head -1)
-#        prov=$(echo "$dataline" | cut -d',' -f5 | head -1)
-        country=$(echo "$dataline" | cut -d',' -f7 | head -1)
+        city=$(echo "$dataline" | cut -d',' -f5 | head -1)
+        prov=$(echo "$dataline" | cut -d',' -f6 | head -1)
+        cntry=$(echo "$dataline" | cut -d',' -f7 | head -1 | tr -d '\r')
 
-	if [ -z "$name" ]; then
-		name="Name N/A"
-	fi
-#echo  "$tm $mode $call $name $country |"
-#line0="$tm $mode $call $name $country |"
-line0="$tm $mode $call $name $country ""\n"
+#	echo "$call $cntry"
 
-#echo -e "---$line0|""\n"
+fi
+  	dtm2=$(date -d "${dtm:0} UTC" '+%H:%M:%S')
 
-      if [ "$mode" == "YSF" ]; then
-	line0="$tm $mode $call""|"
+
+      if [ "$mode" = "YSF" ]; then
+	line0="$dtm2","$mode,""$call|"
+#	line7+="$line0|"
+      fi 
+      if [ "$mode" = "DMR" ]; then
+	line1="$dtm2 $mode $call $name $cntry|"
+#       line1+=" $cntry"
+
+#	line8+="$line1|"
+        
       fi
 	
-	line7+="$line0"
-
-#	echo -e "$line7""\n"
+line9+="$line0"
+line9+="$line1"
 
 #	line3=$(echo  "$dt" "$tm" "$call" "$name" | awk '{printf  "%5s %s %s %s|\n", $1 $2 $3 $name}')
 #	line3=""
-#	echo  "$tm $mode $call $name $country |" |   tr -d "\n"
+#	echo  "$tm $mode $call $name $cntry |" |   tr -d "\n"
 #	echo "$line2"
 #	line4=${line3:0:37}
 	line6=$(echo "$line3" | tr -d "\n")
@@ -109,10 +119,15 @@ line0="$tm $mode $call $name $country ""\n"
 	list5+="$line6"
 done <<< "$list1"
 
-echo -e "$line7"
+echo -e "$line9"
 
+#echo -e "$line7"
+#echo -e "$line8"
+
+#echo -e "\r\n"
+#echo -e "$line7"
 var="${list5:0:400}"
-echo "${var}"
+#echo "${var}"
 #sudo mount -o remount,rw /
 
 #echo "${var}" >> ./lh2_start.txt
@@ -142,7 +157,6 @@ list1=$(echo "$list3" | tail -n 8)
 #echo "  "
 line=$(echo "$list1" | tail -n10)
 #echo "6 $list1"
-
 	domode2
 
 #sudo mount -o remount,ro /
